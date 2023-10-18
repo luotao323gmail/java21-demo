c73eba89195b28699dce3a1e3f77326bb5bb213d
luotao323gmail <luotao323@gmail.com>
2023-10-18 20:32:40 +0900

    op

--- spring1/src/main/java/com/example/spring1/Spring1Application.java
+++ spring1/src/main/java/com/example/spring1/Spring1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring1Application {
 
 	public static void main(String[] args) {
+		System.out.println(123);
 		SpringApplication.run(Spring1Application.class, args);
 	}
 
