463378664b6228c1c267dc1eaf7fd79226b4970f
luotao323gmail <luotao323@gmail.com>
2023-10-18 19:04:10 +0900

    add demo1

---
+++ java21-demo1/src/main/java/com/example/java21demo1/Java21Demo1Application.java
@@ -0,0 +1,13 @@
+package com.example.java21demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java21Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java21Demo1Application.class, args);
+	}
+
+}
